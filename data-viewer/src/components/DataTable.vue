8b33499be1eb6594bd8e7fdbc5b60452625c1d01
dongcodebmt <admin@dongdev.com>
2024-09-16 22:49:06 +0700

    Feat: add loading animation

--- data-viewer/src/components/DataTable.vue
+++ data-viewer/src/components/DataTable.vue
@@ -73,10 +73,19 @@
       </div>
 
       <!-- Kết quả -->
-      <p class="text-gray-700 mb-2">Tổng cộng {{ data?.total ?? 0 }} kết quả</p>
+      <p class="text-gray-700 mb-2">Tổng cộng {{ isLoading ? 0 : data?.total ?? 0 }} kết quả</p>
 
       <!-- Bảng dữ liệu -->
-      <div class="overflow-x-auto">
+      <div class="text-center" v-if="isLoading">
+          <div role="status">
+              <svg aria-hidden="true" class="inline w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+                  <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+              </svg>
+              <span class="sr-only">Loading...</span>
+          </div>
+      </div>
+      <div class="overflow-x-auto" v-if="!isLoading">
         <table class="min-w-full bg-white border rounded-lg shadow-md">
           <thead>
             <tr>
@@ -108,11 +117,11 @@
           </tbody>
         </table>
       </div>
-    </div>
 
-    <div class="mt-6">
-      <TablePagination :currentPage="query.page" :totalPages="data?.totalPages ?? 0"
-        @update:currentPage="updatePage" />
+      <div class="mt-6" v-if="!isLoading">
+        <TablePagination :currentPage="query.page" :totalPages="data?.totalPages ?? 0"
+          @update:currentPage="updatePage" />
+      </div>
     </div>
 
     <div class="flex justify-center mt-4">
@@ -132,6 +141,7 @@ export default {
   },
   data() {
     return {
+      isLoading: true,
       query: {
         search: null,
         startDate: null,
@@ -158,9 +168,57 @@ export default {
     }
   },
   watch: {
-    query: {
+    'query.search': {
       handler() {
         this.debouncedFetchData()
+      },
+      deep: true
+    },
+    'query.min': {
+      handler() {
+        this.debouncedFetchData()
+      },
+      deep: true
+    },
+    'query.max': {
+      handler() {
+        this.debouncedFetchData()
+      },
+      deep: true
+    },
+    'query.startDate': {
+      handler() {
+        this.fetchData()
+      },
+      deep: true
+    },
+    'query.endDate': {
+      handler() {
+        this.fetchData()
+      },
+      deep: true
+    },
+    'query.sortBy': {
+      handler() {
+        this.fetchData()
+      },
+      deep: true
+    },
+    'query.order': {
+      handler() {
+        this.fetchData()
+      },
+      deep: true
+    },
+    'query.page': {
+      handler() {
+        this.fetchData()
+      },
+      deep: true
+    },
+    'query.pageSize': {
+      handler() {
+        this.fetchData()
       },
       deep: true
     }
@@ -168,6 +226,7 @@ export default {
   methods: {
     async fetchData() {
       try {
+        this.isLoading = true
         const response = await axios.get('https://var.dongdev.com/be/api/data', {
           params: {
             search: this.query.search,
@@ -187,6 +246,7 @@ export default {
           this.query.page = 1
         }
         document.title = this.content?.title
+        this.isLoading = false
       } catch (error) {
         console.error('Error fetching data:', error)
       }
@@ -215,7 +275,9 @@ export default {
   },
   created() {
     this.fetchData()
-    this.debouncedFetchData = debounce(this.fetchData, 500)
+    this.debouncedFetchData = debounce(() => {
+      this.fetchData()
+    }, 300)
   }
 }
 </script>
